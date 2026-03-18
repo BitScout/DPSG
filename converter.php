56bc0b3d03a3824abec47d0f72b21986ba3a034c
Christian Kollross <mail@hydranten.eu>
2026-03-18 21:22:25 +0100

    Einrückung im Converter vereinheitlicht

--- converter.php
+++ converter.php
@@ -4,33 +4,33 @@ $input = json_decode(file_get_contents('dpsg.json'));
 
 $output = new stdClass();
 foreach($input as $dioezeseKomplex) {
-	if(!is_object($dioezeseKomplex)) {
-		continue;
-	}
-	
-	$dioezeseEinfach = new stdClass;
-	
-	if(property_exists($dioezeseKomplex, 'bezirke') && is_array($dioezeseKomplex->bezirke)) {
-		foreach($dioezeseKomplex->bezirke as $bezirkKomplex) {
-			if(!is_object($bezirkKomplex)) {
-				continue;
-			}
-			
-			$bezirkEinfach = array();
-			
-			if(property_exists($bezirkKomplex, 'staemme') && is_array($bezirkKomplex->staemme)) {
-				foreach($bezirkKomplex->staemme as $stammKomplex) {
-					if(!is_object($stammKomplex)) {
-						continue;
-					}
-					array_push($bezirkEinfach, $stammKomplex->name);
-				}
-			}
-			
-			$bezirkName = $bezirkKomplex->name;
-			$dioezeseEinfach->$bezirkName = $bezirkEinfach;
-		}
-	}
+    if(!is_object($dioezeseKomplex)) {
+        continue;
+    }
+    
+    $dioezeseEinfach = new stdClass;
+    
+    if(property_exists($dioezeseKomplex, 'bezirke') && is_array($dioezeseKomplex->bezirke)) {
+        foreach($dioezeseKomplex->bezirke as $bezirkKomplex) {
+            if(!is_object($bezirkKomplex)) {
+                continue;
+            }
+            
+            $bezirkEinfach = array();
+            
+            if(property_exists($bezirkKomplex, 'staemme') && is_array($bezirkKomplex->staemme)) {
+                foreach($bezirkKomplex->staemme as $stammKomplex) {
+                    if(!is_object($stammKomplex)) {
+                        continue;
+                    }
+                    array_push($bezirkEinfach, $stammKomplex->name);
+                }
+            }
+            
+            $bezirkName = $bezirkKomplex->name;
+            $dioezeseEinfach->$bezirkName = $bezirkEinfach;
+        }
+    }
 
     $staemmeEinfach = array();
 
@@ -45,8 +45,8 @@ foreach($input as $dioezeseKomplex) {
         $dioezeseEinfach = $staemmeEinfach;
     }
 
-	$dioezeseName = $dioezeseKomplex->name;
-	$output->$dioezeseName = $dioezeseEinfach;
+    $dioezeseName = $dioezeseKomplex->name;
+    $output->$dioezeseName = $dioezeseEinfach;
 }
 
 file_put_contents('dpsg_einfach.json', json_encode($output, JSON_PRETTY_PRINT + JSON_UNESCAPED_UNICODE));
